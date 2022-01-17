643acc46152228297218171fb0efe8659149054e
chenliwen_hand <1410762621@qq.com>
2022-01-17 10:45:39 +0800

    取消路由

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Home></Home>
   </div>
 </template>
+<script>
+import Home from './views/Home.vue'
+export default {
+  components:{
+    Home
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
